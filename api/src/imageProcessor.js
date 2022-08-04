eb52015045869446145b8bfcbdcbd28329982e37
jhouston0411 <john.houston0411@hotmail.com>
2022-08-03 20:28:36 -0500

    second

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -3,15 +3,17 @@ const {Worker, isMainThread} = require('worker_threads');
 
 
 const pathToResizeWorker = path.resolve(__dirname, 'resizeWorker.js')
+const pathToMonochromeWorker = path.resolve(__dirname, 'monochromeWorker.js')
 
-
-
+const uploadPathResolver = (filename) => {
+    return path.resolve(__dirname, '../uploads', filename);
+}
 
 
 
 
 const imageProcessor = () => {
-    return new Promis((resolve, reject) =>{
+    return new Promise((resolve, reject) =>{
         if(isMainThread){
 
         }else{
