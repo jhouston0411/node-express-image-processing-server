505f112ffffe4bccb7ae77fb710f40a149d7e7f4
jhouston0411 <john.houston0411@hotmail.com>
2022-08-03 19:31:17 -0500

    second

--- api/app.js
+++ api/app.js
@@ -3,7 +3,8 @@ const path = require('path');
 
 const app = express();
 
-const pathToIndex = path.resolve(__dirname + '../client/index.html')
+const pathToIndex = path.resolve(__dirname, '../client/index.html');
+
 app.use('/*', (request, responce) =>{
     responce.sendFile(pathToIndex);
 })
