ac1fde72f10ab0cd9b35391b03d93ee9c2c42af6
jhouston0411 <john.houston0411@hotmail.com>
2022-08-03 20:22:50 -0500

    second

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -1,3 +1,25 @@
 const path = require('path');
 const {Worker, isMainThread} = require('worker_threads');
 
+
+const pathToResizeWorker = path.resolve(__dirname, 'resizeWorker.js')
+const 
+
+
+
+
+
+
+const imageProcessor = () => {
+    return new Promis((resolve, reject) =>{
+        if(isMainThread){
+
+        }else{
+            reject(new Error('not on main thread'))
+        }
+
+        resolve();
+    })
+}
+
+module.exports = imageProcessor
